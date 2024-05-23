de7a626729e26730c527f60bb3b561be869e68a0
ay-dex <megha.dexbytes@gmail.com>
2024-05-23 12:17:14 +0530

    created project

---
+++ android/app/src/main/java/com/example/customersupport/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.customersupport;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/customer_support_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomerSupportScreen extends StatefulWidget {
+  const CustomerSupportScreen({super.key});
+
+  @override
+  State<CustomerSupportScreen> createState() => _CustomerSupportScreenState();
+}
+
+class _CustomerSupportScreenState extends State<CustomerSupportScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
