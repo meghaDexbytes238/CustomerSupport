d9efc75a3c9db9dc3a13e001f7129cfc2de3aa6e
ay-dex <megha.dexbytes@gmail.com>
2024-05-24 15:15:20 +0530

    done

--- lib/customer_support_screen.dart
+++ lib/customer_support_screen.dart
@@ -54,12 +54,11 @@ class _CustomerSupportScreenState extends State<CustomerSupportScreen> {
                child: (bloc.msgList.isEmpty)?  Center(child: Text('${AppString.notMsg}'))
                    :
                ListView.builder(
-
                  controller: bloc.controller,
                  itemCount: bloc.msgList.length + 1,
                  itemBuilder: (context, index) {
 
-                   if(index == bloc.msgList.length){
+                   if((index == bloc.msgList.length)){
                      return Container( height: 95.sp,);
                    }
                    return   bloc.msgList[index].isSender == false ?
@@ -157,12 +156,9 @@ class _CustomerSupportScreenState extends State<CustomerSupportScreen> {
              Align(
                alignment: Alignment.bottomCenter,
                child: Container(
-               //  height: MediaQuery.of(context).size.height -60.sp,
                  height: 45.sp,
-                 //height: (MediaQuery.of(context).viewInsets.bottom == 0) ? 45.sp : 0,
                  clipBehavior: Clip.antiAlias,
                  decoration: BoxDecoration(
-                   //color: Colors.transparent,
                    color: Colors.red,
                    boxShadow: [
                      BoxShadow(
@@ -173,7 +169,6 @@ class _CustomerSupportScreenState extends State<CustomerSupportScreen> {
                    ],
                    shape: BoxShape.rectangle,
                    borderRadius: BorderRadius.circular(15.sp),
-                   //color: Colors.red,
                  ),
                  child: Stack(
                    children: [
@@ -209,162 +204,7 @@ class _CustomerSupportScreenState extends State<CustomerSupportScreen> {
 
            ],
          ),
-         /*child: Column(
-           // crossAxisAlignment: CrossAxisAlignment.stretch,
-           // mainAxisSize: MainAxisSize.max,
-           children: [
-             Flexible(
-               child: Container(
-                 height: MediaQuery.of(context).size.height -200,
-                // color: AppColor.red,
-                 child: (bloc.msgList.isEmpty)?  Center(child: Text('${AppString.notMsg}'))
-                     :
-                 ListView.builder(
-                   controller: bloc.controller,
-                   itemCount: bloc.msgList.length,
-                   itemBuilder: (context, index) {
-                     return   bloc.msgList[index].isSender == false ?
-                     InkWell(
-                       child: Row(
-                         mainAxisAlignment: MainAxisAlignment.start,
-                         children: [
-                           CircleAvatar(
-                             backgroundColor: AppColor.circleAvtarBg ,
-                             child: SvgPicture.asset('assets/images/userIcon.svg'),),
-                           Center(
-                             child: Container(
-                               margin: EdgeInsets.symmetric(
-                                   vertical: 5.sp, horizontal: 10.sp),
-                               padding: EdgeInsets.symmetric(
-                                   horizontal: 10.sp, vertical: 7.sp),
-                               decoration: BoxDecoration(
-                                   borderRadius: BorderRadius.circular(20.sp),
-                                   color: AppColor.lightGrey
-                               ),
-                               child: Widgets.customText(data: bloc.msgList[index].message,fontSize: 15.sp,fontWeight: FontWeight.w500)
-                             ),
-                           ),
-                         ],
 
-                       ),
-                       onLongPress: () {
-                         Widgets.alert(context: context,
-                             actions:
-                           [
-                             Widgets.popUpActionBtn(text: 'Ok', onTap: () {
-                               Navigator.pop(context);
-                               bloc.add(ChatDeleteEvent(index: index));
-                             }),
-                             Widgets.popUpActionBtn(text: 'Cancel', onTap: () {
-                               Navigator.pop(context);
-                             }),
-                           ],
-                         );
-                       },
-                     ):
-                     Padding(
-                       padding:  EdgeInsets.only(left: 50.sp),
-                       child: InkWell(
-                         child: Row(
-                           crossAxisAlignment: CrossAxisAlignment.start,
-                           mainAxisAlignment: MainAxisAlignment.end,
-                           children: [
-                             Flexible(
-                               child: Container(
-                                 margin: EdgeInsets.symmetric(
-                                     vertical: 5.sp, horizontal: 10.sp),
-                                 padding: EdgeInsets.symmetric(
-                                     horizontal: 12.sp, vertical: 10.sp),
-                                 decoration: BoxDecoration(
-                                     borderRadius: BorderRadius.circular(20.sp),
-                                     color: AppColor.red
-                                 ),
-                           child: Widgets.customText(data: bloc.msgList[index].message,fontSize: 15.sp,fontWeight: FontWeight.w500,color: AppColor.white)
-
-                               ),
-                             ),
-                             Container(
-                               decoration: BoxDecoration(
-                                   image: const DecorationImage(image: AssetImage('assets/images/userImg.png')),
-                                   borderRadius: BorderRadius.circular(30.sp),
-                                   border: Border.all(width: 2.sp,color: AppColor.red)
-                               ),
-                               height: 38.sp,
-                               width: 38.sp,
-                               margin: EdgeInsets.only(top: 5.sp),
-                             ),
-                           ],
-                         ),
-                         onLongPress: () {
-                           Widgets.alert(
-                             context: context,
-                             actions:
-                             [
-                               Widgets.popUpActionBtn(text: 'Ok', onTap: () {
-                                 Navigator.pop(context);
-                                 bloc.add(ChatDeleteEvent(index: index));
-                               }),
-                               Widgets.popUpActionBtn(text: 'Cancel', onTap: () {
-                                 Navigator.pop(context);
-                               }),
-                             ],
-                           );
-                         },
-                       ),
-                     );
-                   },
-                 ),
-               ),),
-             Container(
-               height: (MediaQuery.of(context).viewInsets.bottom == 0) ? 45.sp : 0,
-               clipBehavior: Clip.antiAlias,
-               decoration: BoxDecoration(
-                 color: Colors.red,
-                 boxShadow: [
-                   BoxShadow(
-                     color: Colors.grey.withOpacity(0.5.sp), //color of shadow
-                     blurRadius: 7,
-                     offset: const Offset(0, 1),
-                   ),
-                 ],
-                 shape: BoxShape.rectangle,
-                 borderRadius: BorderRadius.circular(15.sp),
-                 //color: Colors.red,
-               ),
-               child: Stack(
-                 children: [
-                   Padding(
-                     padding:  EdgeInsets.only(right: 40.sp),
-                     child: TextField(
-                       maxLines: 2,
-                       controller: bloc.textController,
-                       decoration: InputDecoration(
-                         filled: true,
-                         border: InputBorder.none,
-                         fillColor: AppColor.white,
-                         hintText: 'Type here...',
-                         prefixIcon: const Icon(Icons.search),
-                       ),
-                     ),
-                   ),
-                   Align(
-                       alignment: Alignment.bottomRight,
-                       child: Widgets.sendBtn(
-                         onPressed: () {
-                           if(bloc.textController.text.isNotEmpty){
-                             bloc.add(ChatSendBtnEvent());
-                           }
-                           bloc.controller.animateTo( bloc.controller.position.maxScrollExtent, duration: Duration(milliseconds: 200), curve: Curves.linear);
-                         },)
-
-                   ),
-                 ],
-
-
-               ),
-             ),
-           ],
-         ),*/
        );
      },),
     );
